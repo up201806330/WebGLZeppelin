fbb94bbfdd7ebbde4ac30dd8c612383010a7c8ab
Joao Sousa <joaosousa.pt@gmail.com>
2020-04-15 01:44:28 +0100

    Completed 2.1.3 I think

--- Project1/proj/MyVehicle.js
+++ Project1/proj/MyVehicle.js
@@ -8,10 +8,20 @@ class MyVehicle extends CGFobject {
 		super(scene);
         
         this.triangleFig = new MyTriangle(this.scene);
+
+        // this.horizAngle = 0;
+        // this.speed = 0;
+        // this.x = 0;
+        // this.y = 0;
+        // this.z = 0;
+        this.reset();
     }
 
     display() {
         
+        this.scene.translate(this.x, this.y, this.z);
+        this.scene.rotate(this.horizAngle * Math.PI / 180.0, 0, 1, 0);
+
         // Centering / aligning the vehicle (triangle at the moment)
         this.scene.rotate(-90*Math.PI/180,1,0,0);
         this.scene.rotate(45*Math.PI/180,0,0,1);
@@ -19,6 +29,29 @@ class MyVehicle extends CGFobject {
 
         this.triangleFig.display();
 
+    }
+
+    update() {
+        this.horizAngleRad = this.horizAngle * Math.PI / 180;
+
+        this.x += this.speed * Math.sin(this.horizAngleRad);
+        this.z += this.speed * Math.cos(this.horizAngleRad);
+    }
+
+    turn(val) {
+        this.horizAngle += val;
+    }
+
+    accelerate(val) {
+        this.speed += val;
+    }
+
+    reset() {
+        this.horizAngle = 0;
+        this.speed = 0;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     }
 
 }
