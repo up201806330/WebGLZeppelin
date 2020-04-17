1713dedcf7c8224cdd60031f5cbc1d4b04b53b51
Joao Sousa <joaosousa.pt@gmail.com>
2020-04-17 13:02:14 +0100

    Create MyTrapeze.js

---
+++ Project1/proj/MyTrapeze.js
@@ -0,0 +1,28 @@
+/**
+ * MyTrapeze
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTrapeze extends CGFobject {
+    constructor(scene) {
+		super(scene);
+        
+        this.trianglePart = new MyTriangle(this.scene);
+        this.rectanglePart = new MyQuad(this.scene);
+    }
+
+    display() {
+        
+        this.scene.pushMatrix();
+        this.scene.scale(0.5, -0.5, 0.5);
+        this.scene.translate(3, 1, 0);
+        this.trianglePart.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.5, -0.5, 0);
+        this.rectanglePart.display();
+        this.scene.popMatrix();
+
+    }
+}
