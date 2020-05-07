a64327c4d9a663cb45be2e40f44880d4e6c0a312
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-07 18:11:59 +0100

    Another one

--- Project1/proj/Vehicle/MyTriangle.js
+++ Project1/proj/Vehicle/MyTriangle.js
@@ -29,6 +29,14 @@ class MyTriangle extends CGFobject {
 			
 		]
 
+		//Facing Z positive
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
+		];
+
 		//The defined indices (and corresponding vertices)
 		//will be read in groups of three to draw triangles
 		this.primitiveType = this.scene.gl.TRIANGLES;
