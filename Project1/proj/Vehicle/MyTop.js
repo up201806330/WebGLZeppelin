2e64200d97e66ff292713107b9a504baf8682e24
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2020-05-24 14:29:02 +0100

    Refactorring

--- Project1/proj/Vehicle/MyTop.js
+++ Project1/proj/Vehicle/MyTop.js
@@ -24,25 +24,23 @@ class MyTop extends CGFobject {
 
         var ang = (2 * Math.PI) / this.slices;
         
-        this.vertices.push(0, 0, 0);
+        for (let i = 0; i < this.slices - 1; i++) this.indices.push(i, i + 1, this.slices);
+        this.indices.push(this.slices - 1, 0, this.slices);
+        
         this.normals.push(0, 0, 1);
-        this.texCoords.push(0.5,0.5);
+        this.texCoords.push(0.5, 0.5);
+        this.vertices.push(0, 0, 0);
 
         for (let j = 0; j < this.slices; j++) {
-            this.vertices.push(Math.cos(ang * j), Math.sin(ang * j), 0);
             this.normals.push(0, 0, 1);
-            this.texCoords.push(0.5+Math.cos(j*(2*Math.PI)/this.slices)/2);
-  		    this.texCoords.push(0.5-Math.sin(j*(2*Math.PI)/this.slices)/2);
+            
+            this.texCoords.push(0.5 + Math.cos(j * (2 * Math.PI) / this.slices)/2);
+  		    this.texCoords.push(0.5 - Math.sin(j * (2 * Math.PI) / this.slices)/2);
+            
+            this.vertices.push(Math.cos(ang * j), Math.sin(ang * j), 0);
         }
-
-        for (let i = 0; i < this.slices - 1; i++) {
-            this.indices.push(i, i + 1, this.slices);
-        }
-
-        this.indices.push(this.slices - 1, 0, this.slices);
 
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
     }
-
 }
