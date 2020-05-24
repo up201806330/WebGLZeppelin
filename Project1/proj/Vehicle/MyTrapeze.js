987de8e9f48fc13608b2d980963dd3170902540b
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2020-05-24 13:33:02 +0100

    Added comment

--- Project1/proj/Vehicle/MyTrapeze.js
+++ Project1/proj/Vehicle/MyTrapeze.js
@@ -12,17 +12,18 @@ class MyTrapeze extends CGFobject {
     }
 
     display() {
-        
+	    
+        // Triangle
         this.scene.pushMatrix();
         this.scene.scale(0.5, -0.5, 0.5);
         this.scene.translate(3, 1, 0);
         this.trianglePart.display();
         this.scene.popMatrix();
 
+	// Rectangle
         this.scene.pushMatrix();
         this.scene.translate(0.5, -0.5, 0);
         this.rectanglePart.display();
         this.scene.popMatrix();
-
     }
 }
